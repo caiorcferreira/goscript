38527414e7e657c6819b03abd35e1387b135d124
Caio Ferreira <caiorcferreira@gmail.com>
2025-09-11 19:05:10 -0300

    chore: example

--- examples/example.go
+++ examples/example.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"github.com/caiorcferreira/goscript"
-	"github.com/caiorcferreira/goscript/internal/interpreter"
 	"github.com/caiorcferreira/goscript/internal/routines"
 	"strings"
 	"time"
@@ -14,17 +13,12 @@ func main() {
 
 	script := goscript.New()
 
-	//, interpreter.WithConcurrency(3)
-
 	script.
 		In(routines.File("data/example.txt").Read()).
-		Chain(interpreter.Debounce(10 * time.Second)).
-		Chain(interpreter.Parallel(routines.Transform(strings.ToUpper), 3)).
-		//Chain(routines.Transform(strings.ToUpper)).
-		Out(routines.File("data/output_parallel_10.txt").Write())
-	//Chain(routines.Transform(strings.ToUpper), interpreter.WithDebounce(1*time.Second), interpreter.WithConcurrency(3)).
-	//Chain(routines.Transform(strings.ToUpper)).
-	//Out(routines.File("data/output.txt").Write())
+		Chain(routines.Parallel(routines.Transform(strings.ToUpper), 3)).
+		Chain(routines.Debounce(5 * time.Second)).
+		Out(routines.File("data/output/parallel_15.txt").Write())
+
 	//ExecForEach(goscript.HTTP().Get("https://httpbun.com/get?param={{.}}"))
 
 	ctx := context.Background()
