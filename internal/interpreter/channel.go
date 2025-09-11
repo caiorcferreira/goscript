d70102507e171a83a46974f438bb528128c557f5
Caio Ferreira <caiorcferreira@gmail.com>
2025-09-11 19:07:02 -0300

    refact: remove ack

--- internal/interpreter/channel.go
+++ internal/interpreter/channel.go
@@ -1,22 +1,17 @@
 package interpreter
 
 type ChannelPipe struct {
-	in    chan any
-	inACK chan ACK // used to send ACKs back to the sender
-
-	out    chan any
-	outACK chan ACK // used to receive ACKs from the receiver
+	in  chan any
+	out chan any
 
 	done chan struct{}
 }
 
 func NewChanPipe() *ChannelPipe {
 	return &ChannelPipe{
-		in:     make(chan any, 1),
-		inACK:  make(chan ACK, 1),
-		out:    make(chan any, 1),
-		outACK: make(chan ACK, 1),
-		done:   make(chan struct{}),
+		in:   make(chan any, 1),
+		out:  make(chan any, 1),
+		done: make(chan struct{}),
 	}
 }
 
@@ -42,14 +37,6 @@ func (c *ChannelPipe) SetOutChan(cout chan any) {
 
 func (c *ChannelPipe) Chain(p Pipe) {
 	c.out = p.In()
-}
-
-func (c *ChannelPipe) ACK(ack ACK) {
-	c.inACK <- ack
-}
-
-func (c *ChannelPipe) RecACK() <-chan ACK {
-	return c.outACK
 }
 
 func (c *ChannelPipe) Close() error {
--- internal/interpreter/types.go
+++ internal/interpreter/types.go
@@ -10,18 +10,11 @@ type Msg struct {
 	Data any
 }
 
-type ACK struct {
-	ID    string
-	MsgID string
-}
-
 type Pipe interface {
 	In() chan any
 	Out() chan any
 	Done() <-chan struct{}
 	Chain(p Pipe)
-	ACK(ack ACK)
-	RecACK() <-chan ACK
 	io.Closer
 }
 
