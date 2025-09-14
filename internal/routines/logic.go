a5e72b58c9228f5c9007de36a6e468a1cddb5045
Caio Ferreira <caiorcferreira@gmail.com>
2025-09-14 08:53:33 -0300

    feat: reduce routine

--- internal/routines/logic.go
+++ internal/routines/logic.go
@@ -3,7 +3,9 @@ package routines
 import (
 	"context"
 	"github.com/caiorcferreira/goscript/internal/pipeline"
+	"github.com/google/uuid"
 	"log/slog"
+	"reflect"
 )
 
 type TransformRoutine[T, V any] struct {
@@ -42,6 +44,50 @@ func (t *TransformRoutine[T, V]) Start(ctx context.Context, pipe pipeline.Pipe) 
 			return nil
 		case pipe.Out() <- transformedMsg:
 		}
+	}
+
+	return nil
+}
+
+type ReduceRoutine[T, V any] struct {
+	reduce       func(V, T) V
+	currentValue V
+}
+
+func Reduce[T, V any](f func(V, T) V, initialValue V) *ReduceRoutine[T, V] {
+	return &ReduceRoutine[T, V]{reduce: f, currentValue: initialValue}
+}
+
+func (t *ReduceRoutine[T, V]) Start(ctx context.Context, pipe pipeline.Pipe) error {
+	defer pipe.Close()
+
+	slog.Debug("starting reduce routine")
+
+	for msg := range pipe.In() {
+		slog.Debug("reduce received message", "msg", msg)
+
+		// type assertion to T
+		val, ok := msg.Data.(T)
+		if !ok {
+			slog.Error("reduce received message with invalid type", "type", reflect.TypeOf(msg.Data))
+
+			continue
+		}
+
+		t.currentValue = t.reduce(t.currentValue, val)
+
+		slog.Debug("reduced message", "msg", msg, "currentValue", t.currentValue)
+	}
+
+	reducedMsg := pipeline.Msg{
+		ID:   uuid.NewString(),
+		Data: t.currentValue,
+	}
+
+	select {
+	case pipe.Out() <- reducedMsg:
+	case <-ctx.Done():
+		return nil
 	}
 
 	return nil
