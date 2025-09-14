fbcf3efc98b87e8752757f158a360fe632217229
Caio Ferreira <caiorcferreira@gmail.com>
2025-09-14 09:06:23 -0300

    refact: change examples to examlpe funcs

---
+++ examples/example_file_out_test.go
@@ -0,0 +1,33 @@
+package examples_test
+
+import (
+	"context"
+	"github.com/caiorcferreira/goscript"
+	"github.com/caiorcferreira/goscript/internal/routines"
+	"strings"
+	"time"
+)
+
+func ExampleScript_FileOut() {
+	println("This is an example function.")
+
+	script := goscript.New()
+
+	script.
+		FileIn("../data/example.txt").
+		Parallel(routines.Transform(strings.ToUpper), 3).
+		Debounce(time.Millisecond * 10).
+		FileOut("../data/output/parallel_17.txt")
+
+	//Chain(routines.Parallel(routines.Transform(strings.ToUpper), 3)).
+	//Chain(routines.Debounce(5 * time.Second)).
+	//ExecForEach(goscript.HTTP().Get("https://httpbun.com/get?param={{.}}"))
+
+	ctx := context.Background()
+	err := script.Run(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// Output:
+}
--- examples/example_subpipe_test.go
+++ examples/example_subpipe_test.go
@@ -1,4 +1,4 @@
-package main
+package examples
 
 //func ExampleSubpipe() {
 //	println("This is an example function.")
